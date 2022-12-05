264870bb17b76c2ba3746cfc8d6f2e4197cfe900
tinku gupta <tinku.gupta4@gmail.com>
2022-12-06 00:06:13 +0530

    get request completed done

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,29 +1,50 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-    const [users,setUser] = useState([]);
+  const [users, setUser] = useState([]);
 
-    useEffect(()=>{
+  useEffect(() => {
+    loadUsers();
+  },[]);
 
-        loadUsers();
-
-    },[])
-
-    const loadUsers = async() => {
-        const result = await axios.get('http://localhost:3003/users');
-        // console.log(result);  
-        setUser(result.data);
-    }
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3003/users");
+    // console.log(result);
+    setUser(result.data);
+  };
 
   return (
-    <div className='container'>
-      <div className='py-4'>
+    <div className="container">
+      <div className="py-4">
         <h1>Home Page</h1>
-      </div> 
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">name</th>
+              <th scope="col">User Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-  ) 
-}
+  );
+};
 
-export default Home
+export default Home;
